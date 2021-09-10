fe69c9a98c16bcf3cd412a184e3b9ae73128d4b2
Arjay <arjayramiso@gmail.com>
2021-09-10 17:23:53 +0800

    Webflow API collection post endpoint

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -5,13 +5,13 @@ const Webflow = require('webflow-api')
 const api = new Webflow({token: '67461abf8fa602c5eed5ee0054375260b61c0476b4d91d63582c4f7468a2b2cd'})
 
 router.post('/test', (req, res) => {
-	console.log(JSON.stringify(req.body.form_response.answers))
+	console.log(req.body)
 	
 	const item = api.createItem({
   		  collectionId: '613b15722ae9a2ac9ac49c37',
 		  fields: {
-		    'name': 'Exciting blog post title test 1',
-		    'slug': 'exciting-post test 1',
+		    'name': req.body.name,
+		    'slug': req.body.slug,
 		    '_archived': false,
 		    '_draft': false,
 		  },
@@ -21,7 +21,7 @@ router.post('/test', (req, res) => {
 
 
 
-	res.send(JSON.stringify(req.body.form_response.answers))
+	res.send(req.body)
 })
 
 module.exports = router

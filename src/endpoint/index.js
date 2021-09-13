b48598dc25da4903ce314688ed6fac6162ba7701
Arjay <arjayramiso@gmail.com>
2021-09-13 13:37:57 +0800

    Test 2

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = new express.Router()
 const Webflow = require('webflow-api')
 
-const api = new Webflow({token: '67461abf8fa602c5eed5ee0054375260b61c0476b4d91d63582c4f7468a2b2cd'})
+//const api = new Webflow({token: '67461abf8fa602c5eed5ee0054375260b61c0476b4d91d63582c4f7468a2b2cd'})
 
-router.post('/test', (req, res) => {
-	console.log(req)
-	
+router.post('/test/:token/:collectionid', (req, res) => {
+	const token = req.params.token
+	const collectionid = req.params.collectionid
+	console.log(req.body)
+	console.log(`TOKEN:`,token)
+	console.log(`COLLECTION ID:`, collectionid)
 	// const item = api.createItem({
  //  		  collectionId: '613b15722ae9a2ac9ac49c37',
 	// 	  fields: {
@@ -18,10 +21,7 @@ router.post('/test', (req, res) => {
 	// });
 
 	// item.then(i => console.log(i));
-
-
-
-	res.send(req)
+	res.send(req.body)
 })
 
 module.exports = router

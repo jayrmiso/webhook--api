2f2db159b4cdf68b4fca9f109bf4a4021ffdbcc4
Arjay <arjayramiso@gmail.com>
2021-09-13 14:14:06 +0800

    Test 3

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -5,22 +5,24 @@ const Webflow = require('webflow-api')
 //const api = new Webflow({token: '67461abf8fa602c5eed5ee0054375260b61c0476b4d91d63582c4f7468a2b2cd'})
 
 router.post('/test/:token/:collectionid', (req, res) => {
-	const token = req.params.token
+	const tokenid = req.params.token
 	const collectionid = req.params.collectionid
 	console.log(req.body)
-	console.log(`TOKEN:`,token)
+	console.log(`TOKEN:`,tokenid)
 	console.log(`COLLECTION ID:`, collectionid)
-	// const item = api.createItem({
- //  		  collectionId: '613b15722ae9a2ac9ac49c37',
-	// 	  fields: {
-	// 	    'name': req.body.name,
-	// 	    'slug': req.body.slug,
-	// 	    '_archived': false,
-	// 	    '_draft': false,
-	// 	  },
-	// });
+	const api = new Webflow({token: tokenid})
 
-	// item.then(i => console.log(i));
+	const item = api.createItem({
+  		  collectionId: collectionid,
+		  fields: {
+		    'event': req.body.event_id,
+		    'slug': req.body.event_id,
+		    '_archived': false,
+		    '_draft': false,
+		  },
+	});
+
+	item.then(i => console.log(i));
 	res.send(req.body)
 })
 

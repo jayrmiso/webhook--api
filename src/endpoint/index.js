9862c872cf20e9997d6cdbc95fef000ab1d4e3bb
Arjay <arjayramiso@gmail.com>
2021-09-14 08:15:12 +0800

    Test

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = new express.Router()
 const Webflow = require('webflow-api')
-const downloadFile = require('../utils/downloader.js')
 const fs = require("fs");
 var path = require('path');
 
@@ -10,36 +9,14 @@ router.post('/test/:token/:collectionid', (req, res) => {
 
 	const tokenid = req.params.token
 	const collectionid = req.params.collectionid
-	// console.log(JSON.stringify(req.body.form_response.answers[0].file_url))
-	// downloadFile(req.body.form_response.answers[0].file_url)
 	const api = new Webflow({token: tokenid})
-
-
-	// function fileUrl(str) {
- //    if (typeof str !== 'string') {
- //        throw new Error('Expected a string');
- //    }
-
- //    var pathName = path.resolve(str).replace(/\\/g, '/');
-
- //    // Windows drive letter must be prefixed with a slash
- //    if (pathName[0] !== '/') {
- //        pathName = '/' + pathName;
- //    }
-
- //    return encodeURI('file://' + pathName);
-	// };
-	// console.log(fileUrl(__dirname + '/download.html'))
-	// const temp = fileUrl(__dirname + '/download.html')
-	// console.log(temp)
-
 	const item = api.createItem({
        collectionId: collectionid,
 		  fields: {
 		    'name': req.body.event_id,
 		    'slug': req.body.event_type,
 		    'test': {
-		    	'url': `${req.body.form_response.answers.file_url}`,
+		    	'url': req.body.form_response.answers[0].file_url,
 		    },
 		    '_archived': false,
 		    '_draft': false,

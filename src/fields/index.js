212f1646c8dc38a221f5a28b219f485f50048a6d
Arjay <arjayramiso@gmail.com>
2021-09-16 20:52:14 +0800

    Final Testing

--- src/fields/index.js
+++ src/fields/index.js
@@ -18,6 +18,15 @@ const generateObject = (req, callback) =>{
 		console.log(`Adding a new record to a collection with an ID of: ${process.env.WEBFLOW_2}`)
 		Object.assign(obj, {'description': req.body.form_response.answers[0].text})
 		Object.assign(obj, {'image': {'url': req.body.form_response.answers[1].file_url}})
+	}else if(req.params.collectionid == process.env.WEBFLOW_3){
+		console.log(`[Vietnam Microsite 2021] Adding a new record to a collection with an ID of: ${process.env.WEBFLOW_3}`)
+		Object.assign(obj, {'client': req.body.form_response.answers[0].text})
+		Object.assign(obj, {'name': req.body.form_response.answers[0].text})
+		Object.assign(obj, {'view-live-website':req.body.form_response.answers[1].email})
+		Object.assign(obj, {'csr-report':{'url': req.body.form_response.answers[2].file_url}})
+		Object.assign(obj, {'main-project-image':{'url':req.body.form_response.answers[3].file_url}})
+		Object.assign(obj, {'project-details':req.body.form_response.answers[4].text})
+
 	}
 	//Add another if statement for every new typeform fields
 	//Add collectionid value to .env for the if statement
